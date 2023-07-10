ebad343256b8d191544df98e42c62403763195b5
bymyself-yoon <120462702+bymyself-yoon@users.noreply.github.com>
2023-07-10 10:06:19 +0900

    Update ArtsIndex.py

--- ArtsIndex.py
+++ ArtsIndex.py
@@ -130,7 +130,9 @@ def main():
       facilities = filtered_df.iloc[[17, 12, 13, 14, 15, 16]]
       enjoyment = filtered_df.iloc[[26, 18, 19, 20, 21, 22, 23, 24, 25]]
       achievement = filtered_df.iloc[[34, 27, 28, 29, 30, 31, 32, 33]]
-      arts = filtered_df.iloc[[35]]
+      arts = filtered_df.values[[35]]
+
+      arts
       # print(filtered_df)
     
       # write sub-indices
